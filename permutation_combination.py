00e849031949c0f03603a3ed5f1d3ce1c4957dd4
Mazharul77 <mazharul77.cse@gmail.com>
2022-03-18 00:35:10 +0600

    OOP Features are bound here like inherit, method overriding,and so on

---
+++ permutation_combination.py
@@ -0,0 +1,15 @@
+# Itertools uses: permutation, combination...........
+
+print("Let's Facilitate Itertools")
+print("\n\t Permutation:")
+n = 8
+r = 5
+# nPr = (n!/(n-r)!)
+# print("The Permutation Result Is: ", nPr)
+
+# nCr = (n!/(n-r)!*r!)
+# print("\n\t Combination:")
+# print("The Combination Result Is: ", nCr)
+
+# print("{2} - {0} and finally {1}".format("idea", "implementation", "maintained"))
+
