463798fc599f658e23585d7c7ac53e515bda30e9
Mazharul77 <mazharul77.cse@gmail.com>
2022-03-18 00:46:24 +0600

    object, instances with some built_in Functions are implemented

--- obj_built_func.py
+++ obj_built_func.py
@@ -1,7 +1,7 @@
 # Checking Object's classes,Built_in class_Functions ( isinstance(), issubclass() ), and on.....
 # [ Note: no Access Modifier in Python like Java/others oop(default, public, private, protected)
 # but intelligently can be handled ]
-# Python Supports Multiple Inheritances
+# python Supports Multiple Inheritances
 
 class Project:
     p_name = "ERP_Solutions"
@@ -24,7 +24,8 @@ my_module = Module()
 print("The Launched Project:", my_project.p_name)
 print(my_module.pr_desc)
 
-# Now Check Individual Object representing class: It utilizes for big project fast handling
+# Now Check Individual Object representing class:
+# It utilizes for big project fast handling or fast understanding
 print("The Class of Object-my_module is: ", my_module.__class__)
 print("The Class of Object-my_project is: ", my_project.__class__)
 print("The Class of Object-ai_hrm is: ", ai_hrm.__class__)
@@ -46,5 +47,7 @@ print("\t Is my_module-object is Instance of my_project-class? :", isinstance(my
 print("\n\t Is Software-class is subclass of Project-class? :", issubclass(Software, Project))  # True (inherited subclass)
 print("\t Is Software-class is subclass of Software-class? :", issubclass(Software, Software))  # True (sub of own)
 print("\t Is Software-class is subclass of Module-class? :", issubclass(Software, Module))  # False
+
+# More Complex accessing of modules will bw discussed step by step
 
 
