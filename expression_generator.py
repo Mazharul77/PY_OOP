4041e338a83647f58d35b6a3d19c11b7deb7f732
Mazharul77 <mazharul77.cse@gmail.com>
2022-03-21 02:48:27 +0600

    module generations named generator expression as required

--- expression_generator.py
+++ expression_generator.py
@@ -1,7 +1,11 @@
 print(".......Let's Implement Generator Expression:.......\n")
 sequence_ = [12, 123, 11, 23, 56, "Done"]
-gener_exp = (ge for ge in sequence_)
+gener_exp = (ge for ge in sequence_)  # compression within first closing brackets(): is gen_exp
 
 print("The Type Of gener_exp:", type(gener_exp))
 for ge_demons in gener_exp:
     print(ge_demons)
+
+# it's also 1-time use like generator
+# not printed when reaches end or already used
+print(ge_demons.next())
